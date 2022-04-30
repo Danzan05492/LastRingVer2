7c3e351e1b88a62b8a322326caf23aae8772df5c
bairinc0 <bairinc0@gmail.com>
2022-04-30 11:04:12 +0800

    Добавим slug

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # last_ring_web.local
 -------------- Генерируем модель в ларавель со всем
 https://laravel.com/docs/9.x/eloquent
+
+Работа со slug:
+https://github.com/cviebrock/eloquent-sluggable
--- server/example-app/app/Models/Info.php
+++ server/example-app/app/Models/Info.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Info extends Model
 {
     use HasFactory;
+    use Sluggable;
+
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
+    
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
