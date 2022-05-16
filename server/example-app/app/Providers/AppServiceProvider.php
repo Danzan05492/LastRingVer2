e2551959f2b7af6d3fdb6beff6dd2176eee8c5f8
bairinc0 <bairinc0@gmail.com>
2022-05-16 20:40:27 +0800

    Кэширование

--- server/example-app/app/Providers/AppServiceProvider.php
+++ server/example-app/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Cache;
 use App\Models\Info;
 use App\Models\Category;
 
@@ -25,11 +26,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        
         //получаем посты в сайдбар
         view()->composer('layouts.front.sidebar',function($view){
             $view->with('popular_posts',Info::orderBy('views','desc')->limit(3)->get());
             //получаем категории в сайдбар
-            $view->with('popular_cats',Category::withCount('posts')->orderBy('posts_count','desc')->get());
+            //кэшируем категории
+            if (Cache::has('popular_cats')){
+                $popular_cats=Cache::get('popular_cats');
+            }
+            else{
+                $popular_cats=Category::withCount('posts')->orderBy('posts_count','desc')->get();
+                Cache::put('popular_cats',$popular_cats,40);
+            }
+            $view->with('popular_cats',$popular_cats);
         });
 
     }
