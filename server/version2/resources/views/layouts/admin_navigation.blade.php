802fbf25474d6b8e73c45c69ab562820f8578292
Bair <bairinc0@gmail.com>
2022-06-08 20:09:13 +0800

    ссылка на выход из админки

--- server/version2/resources/views/layouts/admin_navigation.blade.php
+++ server/version2/resources/views/layouts/admin_navigation.blade.php
@@ -125,10 +125,10 @@
         <a class="nav-link" data-widget="fullscreen" href="#" role="button">
           <i class="fas fa-expand-arrows-alt"></i>
         </a>
-      </li>
+      </li>     
       <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
+        <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="{{ route('logout') }}" role="button">
+          <i class="fas fa-arrow-circle-left" alt="Выход"></i>
         </a>
       </li>
     </ul>
