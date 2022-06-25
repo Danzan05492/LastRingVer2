d4464da44e094f26884cdb6abb7fe0aacb3e8d90
bairinc0 <bairinc0@gmail.com>
2022-06-25 11:47:49 +0800

    Заморочка с константами?

--- server/version2/app/Models/Note.php
+++ server/version2/app/Models/Note.php
@@ -9,4 +9,13 @@ class Note extends Model
 {
     use HasFactory;
     protected $fillable=['title','description','type','default_length','content'];
+    /**
+     * Константы для типа узла
+     */
+    const INFORMATION=1;
+    const BREAKPOINT=2;
+    public static function getConstants()
+    {
+        return [self::INFORMATION,self::BREAKPOINT];
+    }
 }
