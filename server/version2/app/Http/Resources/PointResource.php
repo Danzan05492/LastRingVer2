f11919f700a4849e4a34c220b59c50c0ec10cd0c
Bair <bairinc0@gmail.com>
2022-07-22 17:11:02 +0800

    Работа с календарём

--- server/version2/app/Http/Resources/PointResource.php
+++ server/version2/app/Http/Resources/PointResource.php
@@ -19,7 +19,16 @@ class PointResource extends JsonResource
             'freedom_id'=>$this->freedom->id,
             'node_id'=>$this->node->id,
             'status'=>$this->status,
-            'note_title'=>$this->node->title
+            'note_title'=>$this->node->title,
+            //Данные для календаря
+            'title'=>$this->node->title,
+            //'start'=>date('D M d Y H:i:s O', strtotime($this->startdate)),
+            'start'=>'Fri Jul 01 2022 00:00:00 GMT+0800 (Иркутск, стандартное время)',
+            //https://overcoder.net/q/119866/%D0%BA%D0%BE%D0%BD%D0%B2%D0%B5%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C-%D0%B4%D0%B0%D1%82%D1%83-php-%D0%B2-%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%82-%D0%B4%D0%B0%D1%82%D1%8B-javascript
+            'backgroundColor'=> '#f56954', 
+            'borderColor'=>  '#f56954', 
+            'url ' => 'https://www.google.com/',
+            'allDay'=>true
         ];
     }
 }
--- server/version2/public/assets/admin/js/calendarLoader.js
+++ server/version2/public/assets/admin/js/calendarLoader.js
@@ -32,111 +32,139 @@
         };
       }
     });
-
-    var calendar = new Calendar(calendarEl, {
-      headerToolbar: {
-        left  : 'prev,next today',
-        center: 'title',
-        right : 'dayGridMonth,timeGridWeek,timeGridDay'
-      },
-      themeSystem: 'bootstrap',
-      //Random default events
-      events: [
-        {
-          title          : 'All Day Event',
-          start          : new Date(y, m, 1),
-          backgroundColor: '#f56954', //red
-          borderColor    : '#f56954', //red
-          allDay         : true
-        },
-        {
-          title          : 'Long Event',
-          start          : new Date(y, m, d - 5),
-          end            : new Date(y, m, d - 2),
-          backgroundColor: '#f39c12', //yellow
-          borderColor    : '#f39c12' //yellow
-        },
-        {
-          title          : 'Meeting',
-          start          : new Date(y, m, d, 10, 30),
-          allDay         : false,
-          backgroundColor: '#0073b7', //Blue
-          borderColor    : '#0073b7' //Blue
-        },
-        {
-          title          : 'Lunch',
-          start          : new Date(y, m, d, 12, 0),
-          end            : new Date(y, m, d, 14, 0),
-          allDay         : false,
-          backgroundColor: '#00c0ef', //Info (aqua)
-          borderColor    : '#00c0ef' //Info (aqua)
-        },
-        {
-          title          : 'Birthday Party',
-          start          : new Date(y, m, d + 1, 19, 0),
-          end            : new Date(y, m, d + 1, 22, 30),
-          allDay         : false,
-          backgroundColor: '#00a65a', //Success (green)
-          borderColor    : '#00a65a' //Success (green)
-        },
-        {
-          title          : 'Click for Google',
-          start          : new Date(y, m, 28),
-          end            : new Date(y, m, 29),
-          url            : 'https://www.google.com/',
-          backgroundColor: '#3c8dbc', //Primary (light-blue)
-          borderColor    : '#3c8dbc' //Primary (light-blue)
-        }
-      ],
-      editable  : true,
-      droppable : true, // this allows things to be dropped onto the calendar !!!
-      drop      : function(info) {
-        // is the "remove after drop" checkbox checked?
-        if (checkbox.checked) {
-          // if so, remove the element from the "Draggable Events" list
-          info.draggedEl.parentNode.removeChild(info.draggedEl);
-        }
-      }
-    });
-
-    calendar.render();
-    // $('#calendar').fullCalendar()
-
-    /* ADDING EVENTS */
-    var currColor = '#3c8dbc' //Red by default
-    // Color chooser button
-    $('#color-chooser > li > a').click(function (e) {
-      e.preventDefault()
-      // Save color
-      currColor = $(this).css('color')
-      // Add color effect to button
-      $('#add-new-event').css({
-        'background-color': currColor,
-        'border-color'    : currColor
-      })
-    })
-    $('#add-new-event').click(function (e) {
-      e.preventDefault()
-      // Get value and make sure it is not null
-      var val = $('#new-event').val()
-      if (val.length == 0) {
-        return
-      }
-
-      // Create events
-      var event = $('<div />')
-      event.css({
-        'background-color': currColor,
-        'border-color'    : currColor,
-        'color'           : '#fff'
-      }).addClass('external-event')
-      event.text(val)
-      $('#external-events').prepend(event)
-
-      // Add draggable funtionality
-      ini_events(event)
-
-      // Remove event from text input
-      $('#new-event').val('')
-    })
+    $.ajax(
+      '/api/points',
+      {
+          success: function(data) {
+            console.log( new Date(y, m, 1));
+            var calendarEvents = [];
+            for (key in data.data) { 
+              var v = data.data[key];
+              calendarEvents.push(JSON.stringify(v));
+            }
+            console.log(calendarEvents);            
+            
+            var calendar = new Calendar(calendarEl, {
+              headerToolbar: {
+                left  : 'prev,next today',
+                center: 'title',
+                right : 'dayGridMonth,timeGridWeek,timeGridDay'
+              },
+              themeSystem: 'bootstrap',
+              //Random default events
+              events: calendarEvents,
+              editable  : true,
+              droppable : true, // this allows things to be dropped onto the calendar !!!
+              drop      : function(info) {
+                // is the "remove after drop" checkbox checked?
+                if (checkbox.checked) {
+                  // if so, remove the element from the "Draggable Events" list
+                  info.draggedEl.parentNode.removeChild(info.draggedEl);
+                }
+              }
+            });
+        
+            calendar.render();
+            // $('#calendar').fullCalendar()
+        
+            /* ADDING EVENTS */
+            var currColor = '#3c8dbc' //Red by default
+            // Color chooser button
+            $('#color-chooser > li > a').click(function (e) {
+              e.preventDefault()
+              // Save color
+              currColor = $(this).css('color')
+              // Add color effect to button
+              $('#add-new-event').css({
+                'background-color': currColor,
+                'border-color'    : currColor
+              })
+            })
+            $('#add-new-event').click(function (e) {
+              e.preventDefault()
+              // Get value and make sure it is not null
+              var val = $('#new-event').val()
+              if (val.length == 0) {
+                return
+              }
+        
+              // Create events
+              var event = $('<div />')
+              event.css({
+                'background-color': currColor,
+                'border-color'    : currColor,
+                'color'           : '#fff'
+              }).addClass('external-event')
+              event.text(val)
+              $('#external-events').prepend(event)
+        
+              // Add draggable funtionality
+              ini_events(event)
+        
+              // Remove event from text input
+              $('#new-event').val('')
+            })
+          },
+          error: function() {
+            alert('There was some error performing the AJAX call!');
+          }
+       }
+    );
+    
   })
+/**
+ * 
+ * {
+                  title          : 'Весь день',
+                  start          : new Date(y, m, 1),
+                  backgroundColor: '#f56954', //red
+                  borderColor    : '#f56954', //red
+                  url            : 'https://www.google.com/',
+                  allDay         : true
+                },
+                {
+                  title          : 'Даь день',
+                  start          : new Date(y, m, 1),
+                  backgroundColor: '#f56954', //red
+                  borderColor    : '#f56954', //red
+                  allDay         : true
+                },
+                {
+                  title          : 'Long Event',
+                  start          : new Date(y, m, d - 5),
+                  end            : new Date(y, m, d - 2),
+                  backgroundColor: '#f39c12', //yellow
+                  borderColor    : '#f39c12' //yellow
+                },
+                {
+                  title          : 'Meeting',
+                  start          : new Date(y, m, d, 10, 30),
+                  allDay         : false,
+                  backgroundColor: '#0073b7', //Blue
+                  borderColor    : '#0073b7' //Blue
+                },
+                {
+                  title          : 'Lunch',
+                  start          : new Date(y, m, d, 12, 0),
+                  end            : new Date(y, m, d, 14, 0),
+                  allDay         : false,
+                  backgroundColor: '#00c0ef', //Info (aqua)
+                  borderColor    : '#00c0ef' //Info (aqua)
+                },
+                {
+                  title          : 'Birthday Party',
+                  start          : new Date(y, m, d + 1, 19, 0),
+                  end            : new Date(y, m, d + 1, 22, 30),
+                  allDay         : false,
+                  backgroundColor: '#00a65a', //Success (green)
+                  borderColor    : '#00a65a' //Success (green)
+                },
+                {
+                  title          : 'Click for Google',
+                  start          : new Date(y, m, 28),
+                  end            : new Date(y, m, 29),
+                  url            : 'https://www.google.com/',
+                  backgroundColor: '#3c8dbc', //Primary (light-blue)
+                  borderColor    : '#3c8dbc' //Primary (light-blue)
+                }
+ */
