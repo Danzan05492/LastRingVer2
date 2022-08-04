fd5d508145ec6fc63082ad7799a0028dafd08610
Bair <bairinc0@gmail.com>
2022-08-04 17:32:20 +0800

    Добавлен метод проверки токена

--- server/version2/routes/mobileAuth.php
+++ server/version2/routes/mobileAuth.php
@@ -3,7 +3,16 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
-
+//метод для проверки авторизован или нет юзер
+Route::get('/sanctum/check',function(Request $request){
+    if (auth('sanctum')->check()){
+        return 1;
+    }
+    else{
+        return -1;
+    }
+});
+//Генерация токена
 Route::post('/sanctum/token', function (Request $request) {
     $request->validate([
         'email' => 'required|email',
