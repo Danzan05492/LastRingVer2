98fc73c931c18472616d2102795a4dde25183149
Bair <bairinc0@gmail.com>
2022-08-15 17:13:48 +0800

    методя для обновления точки

--- server/version2/app/Http/Controllers/Api/PointApiController.php
+++ server/version2/app/Http/Controllers/Api/PointApiController.php
@@ -9,9 +9,26 @@ use App\Http\Resources\PointResource;
 
 class PointApiController extends Controller
 {
+    /**
+     * Метод возвращает в REST список точек для пользователя
+     */
     public function index(Request $request)
     {           
         $points = Point::userPoints($request->user()->id);
         return PointResource::collection($points);
     }
+    /**
+     * Метод получает точку из приложения и обновляет их
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return string - строка с результатом
+     */
+    public function update(Request $request,$id){
+        $point=Point::find($id);    
+        //ТУТ проверять что мы можем обновлять только свои точки
+        $this->authorize('update', $point);            
+        $point->update($request->all());        
+        return "SUCCESS";
+    }
+    
 }
